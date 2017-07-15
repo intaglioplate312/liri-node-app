c1b580bc29c8494b1ac70329db3ebd4ebe0cbd7e
intaglioplate312 <helenelizabeth@me.com>
2017-07-15 18:19:40 -0500

    initial file structure

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'mLNVVLFyfT7BrRfMaDOVzayW4',
+    consumer_secret: '26xyCX05K21XAfPi2ApTsslUhMirSsKugIdSODaaXYtyugOq2l',
+    access_token_key: '886203967516143616-A8IwoPiztTepLxE061DYqP5QeDfV9vq',
+    access_token_secret: '2NGVfNtouTOkDMWllQiFgHJW1D6sItyyFpCJolvQoTjRI',
+}
---
+++ liri.js
@@ -0,0 +1,37 @@
+//At the top of the `liri.js` file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
+
+//8Make it so liri.js can take in one of the following commands:
+
+//my-tweets
+//node liri.js my-tweets
+//This will show your last 20 tweets and when they were created at in your terminal/bash window
+
+//spotify-this-song
+//This will show the following information about the song in your terminal/bash window
+//Artist(s)
+//The song's name
+//A preview link of the song from Spotify
+//The album that the song is from
+//If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+//movie-this
+//This will output the following information to your terminal/bash window:
+//* Title of the movie.
+//* Year the movie came out.
+//* IMDB Rating of the movie.
+//* Rotten Tomatoes Rating of the movie.
+//* Country where the movie was produced.
+//* Language of the movie.
+//* Plot of the movie.
+//* Actors in the movie.
+//If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.
+
+//do-what-it-says
+//Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+//It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+//Feel free to change the text in that document to test out the feature for other commands.
+
+//Bonus
+//In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
+//Make sure you append each command you run to the `log.txt` file. 
+//Do not overwrite your file each time you run a command
