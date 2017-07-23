92a87d336de0b6762e9b372927d289ff017ee730
intaglioplate312 <helenelizabeth@me.com>
2017-07-23 17:05:51 -0500

    turned into meet dealine, needs finessed

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # liri-node-app
 Language_ Interpretation and Recognition Interface
+
+LIRI is a terminal app and its prompts are given in the command line.
+
+Before using this app.
+Please, install the package.JSON file, using the command:
+npm install 
+
+Should that fail please install these npm packages one at a time, using these commands:
+npm install twitter
+npm install --save node-spotify-api
+npm install request
+
+
+Commands to use LIRI
+Please use the following commands:
+
+To see upto 20 of my scintillating tweets use:
+node liri.js my-tweets
+
+To information on a song from Spotify:
+node liri.js spotify-this-song 'song title'
+
+To information on a movie from OMDB:
+node liri.js movie-this 'movie title’
+
+To see some random search:
+node liri.js do-what-it-says 
+
+* please place song and movie titles in quotation marks.
+
--- liri.js
+++ liri.js
@@ -1,103 +1,112 @@
-//At the top of the `liri.js` file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
-var dataKeys = require("./keys.js");
-var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
-var request = require('request');
-var fs = require('fs'); //file system
+// node.js variables
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
+var request = require("request");
+var dataKeys = require("./keys.js")
 
+// arguments
 var action = process.argv[2];
 var value = process.argv[3];
 
-
-//my-tweets
-function myTweets() {
-    //pulling keys from file
-    var client = new twitter(dataKeys.twitterKeys);
-    //show your last 20 tweets and when they were created 
-    var params = { screen_name: 'JustoffLSD', count: 20 };
-
-    client.get('https://api.twitter.com/1.1/statuses/user_timeline.json', function(error, tweets, response) {
-        tweets.forEach(function(element) {
-            console.log(element.text + '     ' + "\n" + element.created_at);
-            console.log("\n--------------------------------------------------------");
-        });
-    });
-};
-
-//spotify-this-song
-
-function spotifyThis () {
-      //pulling keys from file
-    var client = new twitter(dataKeys.twitterKeys);
-};
-  
-//This will show the following information about the song in your terminal/bash window
-//Artist(s)
-//The song's name
-//A preview link of the song from Spotify
-//The album that the song is from
-//If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-//movie-this
-function movieThis(value) {
-    var omdbURL = "http://www.omdbapi.com/?apikey=40e9cece&t=";
-    //If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.
-    if (!value) {
-        value = 'Mr. Nobody';
-
-        request(omdbURL + value + '&tomatoes=true&r=json', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                jsonBody = JSON.parse(body);
-                console.log('================================================ ');
-                console.log('Title: ' + jsonBody.Title);
-                console.log('Year: ' + jsonBody.Year);
-                console.log('IMDb Rating: ' + jsonBody.imdbRating);
-                console.log('Rotten Tomatoes Rating: ' + jsonBody.tomatoRating);
-                console.log('Language: ' + jsonBody.Language);
-                console.log('Plot: ' + jsonBody.Plot);
-                console.log('Actors: ' + jsonBody.Actors);
-                console.log('================================================ ');
-
-            };
-        });
-    };
-};
-//do-what-it-says
-//Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-function doWhatItSays() {
-    fs.readFile('./random.txt', 'utf8', function(error, data) {
-        if (error) {
-            console.log(error);
-        } else {
-            var dataArr = data.split(',');
-            if (dataArr[0] === 'spotify-this-song') {
-                spotifyThis(dataArr[1]);
-            }
-            if (dataArr[0] === 'movie-this') {
-                omdbThis(dataArr[1]);
-            }
+// keys
+var client = new Twitter({
+    consumer_key: dataKeys.twitterKeys.consumer_key,
+    consumer_secret: dataKeys.twitterKeys.consumer_secret,
+    access_token_key: dataKeys.twitterKeys.access_token_key,
+    access_token_secret: dataKeys.twitterKeys.access_token_secret
+});
+var spotify = new Spotify({
+    id: dataKeys.spotifyKeys.clientId_key,
+    secret: dataKeys.spotifyKeys.clientSecret_key
+});
+// spotifyThis funciton
+function spotifyThis() {
+    spotify.search({ type: 'track', query: value }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        for (var i = 0; i < 3; i++) {
+            console.log(" ");
+            console.log("Artist(s): " + data.tracks.items[i].artists[0].name);
+            console.log("Song Name: " + data.tracks.items[i].name);
+            console.log("Spotify Link: " + data.tracks.items[i].preview_url);
+            console.log("Album: " + data.tracks.items[i].album.name);
+            console.log("==================================================" + "\n");
         }
     });
 };
-
+// omdb
+function movieThis(body) {
+    var jsonBody = JSON.parse(body);
+    console.log('');
+    console.log('Title: ' + jsonBody.Title);
+    console.log('Year: ' + jsonBody.Year);
+    console.log('IMDb Rating: ' + jsonBody.imdbRating);
+    console.log('Rotten Tomatoes Rating: ' + jsonBody.tomatoRating);
+    console.log('Country:' + ' ' + jsonBody.Country);
+    console.log('Language: ' + jsonBody.Language);
+    console.log('Plot: ' + jsonBody.Plot);
+    console.log('Actors: ' + jsonBody.Actors);
+    console.log("==================================================" + "\n");
+};
 switch (action) {
     //node liri.js my-tweets
-    case 'my-tweets':
-        myTweets();
+    case 'tweet-me':
+        if (action && value) {
+            console.log("Only 'node liri.js tweet-me' needed.");
+            return;
+        }
+        client.get('https://api.twitter.com/1.1/statuses/user_timeline.json', { screen_name: 'JustoffLSD', count: 20 }, function(error, tweets, response) {
+            tweets.forEach(function(element) {
+                console.log(element.text + "\n" + element.created_at);
+                console.log("==================================================" + "\n");
+            });
+        });
         break;
+        //node liri.js spotify-this-song
     case 'spotify-this-song':
-        spotifyThis(value);
+        if (!value) {
+            spotify.search({ type: 'track', query: 'The Sign, Ace of Base' }, function(err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                console.log(" ");
+                console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name);
+                console.log("Song Name: " + data.tracks.items[0].name);
+                console.log("Spotify Link: " + data.tracks.items[0].preview_url);
+                console.log("Album: " + data.tracks.items[0].album.name);
+                console.log("==================================================" + "\n");
+            });
+            return;
+        }
+        spotifyThis();
         break;
-    //node liri.js movie-this
+        //node liri.js movie-this
     case 'movie-this':
-        movieThis(value);
+        if (!value) {
+            request("http://www.omdbapi.com/?apikey=40e9cece&t=" + "Mr. Nobody" + '&tomatoes=true&r=json', function(error, response, body) {
+                movieThis(body);
+            });
+            return;
+        }
+        request("http://www.omdbapi.com/?apikey=40e9cece&t=" + value + '&tomatoes=true&r=json', function(error, response, body) {
+            movieThis(body);
+        });
         break;
-    //run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
     case 'do-what-it-says':
-        doWhatItSays();
+        fs.readFile('./random.txt', 'utf8', function(err, data) {
+            var dataArr = data.split(',');
+            value = dataArr[1];
+            if (dataArr[0] === "spotify-this-song") {
+                spotifyThis(dataArr[1]);
+            } else if (dataArr[0] === "movie-this") {
+                movieThis(dataArr[1]);
+            }
+        });
+        break;
+        //When somehting goes wrong...
+    default:
+        console.log('... something went vey very wrong');
         break;
 };
-//Bonus
-//In addition to logging the data to your terminal/bash window, output the data to a .txt file called `log.txt`.
-//Make sure you append each command you run to the `log.txt` file. 
-//Do not overwrite your file each time you run a command
